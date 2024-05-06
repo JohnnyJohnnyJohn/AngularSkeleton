33867b8a9703658cf9d5a06035cd69863c22ef01
JoyBoy <fsedjai@dev-id.fr>
2024-05-06 17:03:00 +0200

    dark mode service

--- src/app/core/services/dark-theme.service.ts
+++ src/app/core/services/dark-theme.service.ts
@@ -4,9 +4,19 @@ import {Injectable, signal} from '@angular/core';
   providedIn: 'root'
 })
 export class DarkThemeService {
-  public isDarkTheme = signal<boolean>(true);
+  public isDarkTheme = signal<boolean>(
+    (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ?? true
+  );
 
   toggleTheme() {
     this.isDarkTheme.set(!this.isDarkTheme());
+    localStorage.setItem('dark-theme', this.isDarkTheme().toString());
+  }
+
+  constructor() {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+      this.isDarkTheme.set(e.matches);
+      localStorage.setItem('dark-theme', e.matches.toString());
+    });
   }
 }
